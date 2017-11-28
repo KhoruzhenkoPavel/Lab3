5b9ae1a9cc84a3e312c9464e8b57033fffca36af
11111 <11111@11111->
2017-11-28 05:37:58 +0300

    Добавил 3 пользователя

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main() {
 	string name;
-	cout << "Hello world! " << endl << "login ..." << endl;
+	string a = "Pavel";
+	string b = "Admin";
+	string c = "Anton";
+	cout << "Hello world! " << endl << "login ..." ;
 	cin >> name;
-	cout << "hello, " << name << endl;
+	if (name == a)
+		cout << "Hello, " << a << ". ADMIN" << endl;
+	else if (name == b)
+		cout << "Hello, " << b << ". ADMIN" << endl;
+	else if (name == c)
+		cout << "Hello, " << c << ". ADMIN" << endl;
+	else {
+		cout << "hello, " << name <<" User" <<endl;
+	}
 	return 0;
 }
